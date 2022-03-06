9b37c24ee91739d9004abd1f49cce9ae08f03a16
William Di Luigi <williamdiluigi@gmail.com>
2022-03-06 20:09:43 +0100

    Update release scripts

--- release_prepare.sh
+++ release_prepare.sh
@@ -4,5 +4,3 @@ rm -rf dist/
 python3 setup.py bdist_wheel
 
 python3 -m pip install --user --upgrade twine
-
-# python3 -m twine upload dist/*
---
+++ release_upload.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 -m twine upload dist/*
