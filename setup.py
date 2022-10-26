20dcfd4d8fc8bd04702f27113a360483352ae76c
William Di Luigi <williamdiluigi@gmail.com>
2022-10-27 00:13:16 +0200

    Bump version to 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mailipy",
-    version="0.0.6",
+    version="0.0.7",
     author="William Di Luigi",
     author_email="williamdiluigi@gmail.com",
     description="Bulk send emails easily",
