0dee1f4b40c7ba6fd29c3ba76c85202a408c91c9
William Di Luigi <williamdiluigi@gmail.com>
2022-11-24 00:39:22 +0100

    Update send.py

--- mailipy/send.py
+++ mailipy/send.py
@@ -4,7 +4,6 @@ import argparse
 import email
 import getpass
 import os
-import random
 import shutil
 import smtplib
 import ssl
@@ -20,7 +19,7 @@ def main():
     parser.add_argument("sent", nargs="?", default="sent", help="the folder where to move the emails sent (default: sent)")
     parser.add_argument("--continue", help="continue sending, even if the 'sent' folder is not empty", action="store_true", dest="continue_")
     parser.add_argument("--ssl", help="SSL mode to use", choices=["auto", "none", "starttls", "ssl"], default="auto")
-    parser.add_argument("--sleep", help="sleep a few second after send every mail", default=0)
+    parser.add_argument("--sleep", help="seconds to wait after each sent email", default=0)
     args = parser.parse_args()
 
     if (not os.path.isdir(args.outbox)) or len(os.listdir(args.outbox)) == 0:
@@ -73,7 +72,7 @@ def main():
     send_emails(server, emails, args.outbox, args.sent, args.sleep)
 
 
-def send_emails(server, emails, outbox_dir, sent_dir ,sleep):
+def send_emails(server, emails, outbox_dir, sent_dir, sleep):
     # Create sent folder if necessary
     if not os.path.exists(sent_dir):
         os.mkdir(sent_dir)
@@ -98,13 +97,11 @@ def send_emails(server, emails, outbox_dir, sent_dir ,sleep):
 
             # On success, move the message from the outbox to the sent folder
             shutil.move(os.path.join(outbox_dir, eml), os.path.join(sent_dir, eml))
-        except :
+        except:
             print("[!] Error when sending email to %s" % (msg["To"]))
 
-        sleep= int(sleep)
         if sleep > 0:
             time.sleep(sleep)
-        print("sleep"+str(sleep))
 
 
 if __name__ == "__main__":
